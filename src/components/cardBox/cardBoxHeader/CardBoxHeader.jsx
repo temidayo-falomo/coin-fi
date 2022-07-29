2d34a6a7c9c1da5cb3aedffd50780a481566e1a9
temidayo-falomo <temmideee@gmail.com>
2022-07-29 12:54:08 +0100

    enabled a refresh thingy

--- src/components/cardBox/cardBoxHeader/CardBoxHeader.jsx
+++ src/components/cardBox/cardBoxHeader/CardBoxHeader.jsx
@@ -1,18 +1,18 @@
-import React from 'react'
-import './CardBoxHeader.css'
+import React from "react";
+import "./CardBoxHeader.css";
 
 function CardBoardHeader() {
   return (
-    <header className='header'>
-        <h3>Top 200 Currencies</h3>
-        <ul>
-            <li>All</li>
-            <li>Coins</li>
-            <li>Tokens</li>
-            <li>Popular</li>
-        </ul>
+    <header className="header">
+      <h3>Top 200 Currencies</h3>
+      <ul>
+        <a href="/">All</a>
+        <li>Coins</li>
+        <li>Tokens</li>
+        <li>Popular</li>
+      </ul>
     </header>
-  )
+  );
 }
 
-export default CardBoardHeader
+export default CardBoardHeader;
